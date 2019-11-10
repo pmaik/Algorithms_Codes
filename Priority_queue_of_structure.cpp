57f2bb43a358dc6b1bb392651e722313d060a9b3
Maneesh Kumar Patel <44333326+pmaik@users.noreply.github.com>
2019-11-11 01:32:07 +0530

    Add files via upload

---
+++ Priority_queue_of_structure.cpp
@@ -0,0 +1,51 @@
+/*
+    maneesh(maik)
+*/ 
+// dijikstra algorithm(Shortest path) for undirected graph 
+
+#include<iostream>
+#include<algorithm>
+#include<string>
+#include<bitset>
+#include<deque>
+#include<iterator>
+#include<list>
+#include<map>
+#include<queue>
+#include<set>
+#include<stack>
+#include<unordered_map>
+#include<unordered_set>
+#include<vector>
+using namespace std;
+#define ll long long int
+#define pb push_back
+#define pf push_front
+#define M 1000000007
+#define fastio ios_base::sync_with_stdio(false);
+
+struct data
+{
+    ll nod;
+    ll val;
+};
+
+// comperator function for priority queue 
+struct comp
+{
+    bool operator()(data const& s1, data const& s2)
+    {
+        return s1.val > s2.val;
+    }
+};
+
+int main()
+{
+    fastio;
+    
+    // define priority queue of structure 
+    priority_queue<data, vector<data>, comp>pq;
+    
+    
+    return 0;
+} 
