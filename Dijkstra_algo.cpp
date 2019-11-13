ce4c6744c2e12ddf38714dff305472db180c3dcd
Maneesh Kumar Patel <44333326+pmaik@users.noreply.github.com>
2019-11-13 11:22:55 +0530

    Update Dijkstra_algo.cpp

--- Dijkstra_algo.cpp
+++ Dijkstra_algo.cpp
@@ -1,33 +1,6 @@
 /*
     maneesh(maik)
 */ 
-// dijkstra algorithm(Shortest path) for undirected graph 
-
-#include<iostream>
-#include<algorithm>
-#include<string>
-#include<bitset>
-#include<deque>
-#include<iterator>
-#include<list>
-#include<map>
-#include<queue>
-#include<set>
-#include<stack>
-#include<unordered_map>
-#include<unordered_set>
-#include<vector>
-using namespace std;
-#define ll long long int
-#define pb push_back
-#define pf push_front
-#define M 1000000007
-#define fastio ios_base::sync_with_stdio(false);
-
-
-/*
-    maneesh(maik)
-*/ 
 // Dijkstra algo 
 // The shortest path for undirected
 // you can make this directed  
