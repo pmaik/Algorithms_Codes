8a5114b8f62edb9fd139f1f5c431e8544c02757d
Maneesh Kumar Patel <44333326+pmaik@users.noreply.github.com>
2020-10-19 14:14:31 +0530

    Update KMP_lps.cpp

--- KMP_lps.cpp
+++ KMP_lps.cpp
@@ -29,22 +29,65 @@ using namespace std;
 
 vector<ll> LPS_function(string s) 
 {
-    int n = s.size();
-    vector<ll> lps(n);
-
-    for (int i = 1; i < n; i++) 
-    {
-        int j = lps[i-1];
-
-        while (j > 0 && s[i] != s[j])
-            j = lps[j-1];
-
-        if (s[i] == s[j])
-            j++;
-
-        lps[i] = j;
+    int i, j, n = s.size();
+    vector<int>lps(n);
+    lps[0] = 0;
+    
+    i = 1, j = 0;
+    while(i < n){
+        if(s[i] == s[j]){
+            lps[i] = j + 1;
+            i++; j++;
+        }
+        else{
+            if(j == 0){
+                lps[i] = 0;
+                i++;
+            }
+            else{
+                j = lps[j-1];
+            }
+        }
     }
-    return lps;
+    
+    retunr lps;
+}
+
+int find_Pat(string text, string pat){
+    
+    int n = text.size();
+    int m = pat.size();
+    
+    vector<ll>lps = LPS_function(pat);
+    
+    // LPS ARRAY....
+    for(int i=0; i<m; i++){
+        cout<<lps[i]<<" ";
+    }cout<<endl;
+    
+    int idx = -1;
+    i = 0; j = 0;
+    
+    while( i < n && j < m ){
+        
+        if(text[i] == pat[j]){
+            i++; j++;
+            if(j == m){
+                idx = i-m;
+                break;
+            }
+        }
+        else{
+            if(j == 0){
+                i++;
+            }
+            else{
+                j = lps[j-1];
+            }
+        }
+    }
+    
+    return idx;
 }
 
  
@@ -52,13 +95,17 @@ int main()
 {
     fastio;
 
-    string s;
-    cin>>s;
-
-    vector<ll>lps=LPS_function(s);
-    for(int i=0; i<s.size(); i++)
-    {
-        cout<<lps[i]<<" ";
+    string text, pat;
+    cin>>text;
+    cin>>pat;
+    
+    int idx = find_Pat(text, pat);
+    
+    if( idx != -1 ){
+        cout<<"Pattern found at index: "<<idx<<endl;
+    }
+    else{
+        cout<<"Pattern does not found "<<endl;
     }
 
     return 0;
